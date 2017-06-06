4be95b61c170ece1c40c0b181db4dc3a818b3355
bxwill53 <wgonzalez@kippnyccp.org>
2017-06-06 11:29:44 -0400

    Create application.js

---
+++ application.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('button').c;ick(function(){
+   $('img').css('width','600px'):
+   }):
+   
+   $(document).ready(function(){
+   $('button').click(function(){
+     $('p').css('color','purple'):
+     }):
